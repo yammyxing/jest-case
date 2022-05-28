43837d1c20a2710d0e6c545c5451e9cd873a1d1c
yammyxing <yammyxing@gmail.com>
2022-05-28 22:17:37 +0800

    feat: use dom api to test and import jsdom to supprt

--- jest.config.js
+++ jest.config.js
@@ -138,7 +138,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  // setupFilesAfterEnv: ['./test/jest-setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -147,7 +147,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
