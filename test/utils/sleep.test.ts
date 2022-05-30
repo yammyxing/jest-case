080c311900ef6501e8eeede4a44e72dfea78db00
185182 <185182@cn.chervongroup.com>
2022-05-30 18:34:19 +0800

    feat: mock timer in event-loop

---
+++ test/utils/sleep.test.ts
@@ -0,0 +1,26 @@
+import sleep from "@/utils/sleep";
+
+describe("sleep", () => {
+    beforeEach(() => {
+        jest.useFakeTimers();
+    })
+    it("should sleep 1000ms", async () => {
+        const callback = jest.fn();
+        const act = async () => {
+            await sleep(1000);
+            callback();
+        }
+
+        const promise = act();
+
+        // act();
+
+        expect(callback).not.toHaveBeenCalled();
+
+        jest.runAllTimers();
+
+        await promise;
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    })
+})
