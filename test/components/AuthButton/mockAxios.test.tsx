4d8dd87b19de3f2f694b3d5bfbf776bea1d0833c
185182 <185182@cn.chervongroup.com>
2022-06-02 14:04:23 +0800

    feat: mock http request

--- test/components/AuthButton/mockAxios.test.tsx
+++ test/components/AuthButton/mockAxios.test.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react'
 import AuthButton from '@/components/AuthButton'
-import * as userUtils from 'apis/user';
-import { AxiosResponse } from 'axios'
+import server from '../../mockServer/server';
+import { rest } from 'msw'
+import { UserRoleType } from 'apis/user';
+import axios from "axios";
+
+const setup = (userType: UserRoleType) => {
+    server.use(
+        rest.get('https://mysite.com/api/role', async (req, res, ctx) => {
+            return res(ctx.status(200), ctx.json({userType}))
+        })
+    )
+}
 
 describe('AuthButton mock axios', () => {
     it('should display plain user content', async () => {
-        jest.spyOn(userUtils, 'getUserRole').mockResolvedValueOnce({
-            data: {
-                userType: 'user',
-            }
-        } as AxiosResponse)
+        // setup("user")
+        // jest.spyOn(userUtils, 'getUserRole').mockResolvedValueOnce({
+        //     data: {
+        //         userType: 'user',
+        //     }
+        // } as AxiosResponse)
+        jest.spyOn(axios, "get").mockResolvedValueOnce({
+            data: { userType: "user" },
+        });
 
         render(<AuthButton>hello</AuthButton>)
 
@@ -18,11 +32,15 @@ describe('AuthButton mock axios', () => {
     })
 
     it('should display admin user content', async () => {
-        jest.spyOn(userUtils, 'getUserRole').mockResolvedValueOnce({
-            data: {
-                userType: 'admin',
-            }
-        } as AxiosResponse)
+        // setup("admin")
+        // jest.spyOn(userUtils, 'getUserRole').mockResolvedValueOnce({
+        //     data: {
+        //         userType: 'admin',
+        //     }
+        // } as AxiosResponse)
+        jest.spyOn(axios, "get").mockResolvedValueOnce({
+            data: { userType: "admin" },
+        });
 
         render(<AuthButton>hello</AuthButton>)
 
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,6 +1,19 @@
 import "jest-location-mock";
 import mockConsole from "jest-mock-console";
 import '@testing-library/jest-dom'
+// import server from "./mockServer/server";
+
+// beforeAll(() => {
+//     server.listen();
+// })
+
+// afterEach(() => {
+//     server.resetHandlers();
+// })
+
+// afterAll(() => {
+//     server.close();
+// })
 
 mockConsole();
 
